a1d569e1e3d0188b60cfb6405f9ad0ea3d8a9e7e
Kieran Gallagher <kierano777@hotmail.co.uk>
2024-03-11 15:08:05 +0000

    materialise elements added to base template, static folder created

--- cookbook/routes.py
+++ cookbook/routes.py
@@ -4,4 +4,4 @@ from cookbook.models import Recipe, Ingredient
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("recipe.html")
---
+++ cookbook/static/js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);});
