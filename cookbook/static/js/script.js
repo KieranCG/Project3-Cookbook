d0ecd0177eada61ed2a88da0d3cf5e0456737fd4
Kieran Gallagher <kierano777@hotmail.co.uk>
2024-03-13 07:00:13 +0000

    add recipe html and route created and tested.

--- cookbook/static/js/script.js
+++ cookbook/static/js/script.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
-    M.Sidenav.init(sidenav);});
+    M.Sidenav.init(sidenav);
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
+});
