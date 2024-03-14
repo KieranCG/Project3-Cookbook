53e2c3d84afd2b0e576a4dccc359f6416d88feb1
Kieran Gallagher <kierano777@hotmail.co.uk>
2024-03-14 07:47:19 +0000

    defensive modal created for the delete recipe button

--- cookbook/static/js/script.js
+++ cookbook/static/js/script.js
@@ -10,4 +10,22 @@ document.addEventListener("DOMContentLoaded", function() {
     // collapsible initializataion
     let collapsibles = document.querySelectorAll(".collapsible");
     M.Collapsible.init(collapsibles);
+
+    // Initialize the delete modal
+    let deleteModal = document.getElementById('deleteModal');
+    let deleteModalInstance = M.Modal.init(deleteModal);
+
+    // Handle delete button clicks
+    let deleteButtons = document.querySelectorAll('.delete-btn');
+
+    deleteButtons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            event.preventDefault();
+            var recipeId = this.getAttribute('data-recipe-id');
+            var deleteUrl = "/delete_recipe/" + recipeId; // Construct the delete URL
+            var deleteButton = document.getElementById('deleteButton');
+            deleteButton.setAttribute('href', deleteUrl);
+            deleteModalInstance.open();
+        });
+    });
 });
